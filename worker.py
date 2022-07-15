374f25cae31606f21f62089ecf22a72198eb1a0d
ilias-kanchan <49742118+ilias-kanchan@users.noreply.github.com>
2022-07-15 16:59:31 +0600

    Update worker.py

--- worker.py
+++ worker.py
@@ -19,27 +19,32 @@ class Worker:
 
             x = datetime.datetime.now()
             today_date = x.strftime("%Y_%m_%d")
+            try:
+                client = paramiko.client.SSHClient()
+                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                client.connect(server_ip, username=username, password=password)
+                stdin, stdout, stderr = client.exec_command("hostname")
+            except:
+                print("Wrong username and password")
+                pass
+            else:
 
-            client = paramiko.client.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(server_ip, username=username, password=password)
-            stdin, stdout, stderr = client.exec_command("hostname")
-            # print(stdout.read().decode())
-            hostname = stdout.read().decode().strip('\n')
-            hostname_date = hostname + "_" + today_date
-            # print(hostname_date.strip('\n'))
-            log_path = './log'
-            isexist = os.path.exists(log_path)
-            if not isexist:
-                os.makedirs(log_path)
-            # print(hostname_date)
-            stdin, stdout, stderr = client.exec_command(self.command)
-            command_info = stdout.read().decode()
-            # print(cpu_utilization)
-            with open(f"{log_path}/{hostname_date}.txt", "a+") as f:
-                x = datetime.datetime.now()
-                today_date_time = x.strftime("%Y_%m_%d_%H_%M_%S_yyyy_mm_dd_hr_min_sec")
-                f.write(f"=======================Today is {today_date_time}====================running command===== {self.command}\n{command_info}")
+                # print(stdout.read().decode())
+                hostname = stdout.read().decode().strip('\n')
+                hostname_date = hostname + "_" + today_date
+                # print(hostname_date.strip('\n'))
+                log_path = './log'
+                isexist = os.path.exists(log_path)
+                if not isexist:
+                    os.makedirs(log_path)
+                # print(hostname_date)
+                stdin, stdout, stderr = client.exec_command(self.command)
+                command_info = stdout.read().decode()
+                # print(cpu_utilization)
+                with open(f"{log_path}/{hostname_date}.txt", "a+") as f:
+                    x = datetime.datetime.now()
+                    today_date_time = x.strftime("%Y_%m_%d_%H_%M_%S_yyyy_mm_dd_hr_min_sec")
+                    f.write(f"=======================Today is {today_date_time}====================running command===== {self.command}\n{command_info}")
 
-            stdin.close()
-            client.close()
+                stdin.close()
+                client.close()
