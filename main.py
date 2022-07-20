2f04d586a9eddb1817dcdfcf203069b0078b1cdc
ilias-kanchan <49742118+ilias-kanchan@users.noreply.github.com>
2022-07-20 11:47:38 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from worker import Worker
+from mailer import Mailer
 import csv
+import datetime
 
 filename = 'hosts.csv'
 
@@ -18,3 +20,45 @@ with open(filename, 'r') as csvfile:
 
         disk = Worker("df -h", server, user, password)
         disk.info_collector()
+
+        cpu_utilization = Mailer("grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'", server, user, password)
+        cpu_utilization.cpu_mailer()
+
+        memory_utilization = Mailer("free -m | grep 'Mem' | awk '{Mem=($2-($4+$7))/$2 * 100} END {print Mem}'", server, user, password)
+        memory_utilization.memory_mailer()
+
+        try:
+            with open("today.txt") as f:
+                today = f.read()
+        except FileNotFoundError:
+            with open("today.txt", "w") as f:
+                x = datetime.datetime.now()
+                today_date = x.strftime("%Y%m%d")
+                f.write(today_date)
+        else:
+            x = datetime.datetime.now()
+            today_date = x.strftime("%Y%m%d")
+            if today_date == today:
+                print("date matched")
+            else:
+                disk_utilization = Mailer("df -h | awk '{print $5 $6}' | grep -v 'Use'", server, user, password)
+                disk_utilization.high_disk_utilization()
+
+    with open("today.txt") as f:
+        today = f.read()
+
+    x = datetime.datetime.now()
+    today_date = x.strftime("%Y%m%d")
+
+    if today_date == today:
+        print("date matched")
+    else:
+        disk_utilization_mailer = Mailer("df -h | awk '{print $5 $6}' | grep -v 'Use'", server, user, password)
+        disk_utilization_mailer.high_disk_mailer()
+        with open("today.txt", "w") as f:
+            x = datetime.datetime.now()
+            today_date = x.strftime("%Y%m%d")
+            f.write(today_date)
+
+        with open("high_disk_utilization.txt", "w") as f:
+            f.write("")
